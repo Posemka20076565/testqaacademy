bc81ba55a5237956e025fe4e94edfd9597fb6bc4
amish <andy.aka.shaman@gmail.com>
2023-06-08 19:52:30 +0300

    Help from Andrii. Need to add option options.addArguments("--remote-allow-origins=*"); for mac fraimwork. Next step - add method to manipulate with cookie form

--- src/test/java/logintest/LoginTest.java
+++ src/test/java/logintest/LoginTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,9 @@ import java.util.concurrent.TimeUnit;
 public class LoginTest {
     @Test
     public void validLogIn() throws InterruptedException {
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver webDriver = new ChromeDriver(options);
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
